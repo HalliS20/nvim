b5307e69485d7ee9b48c1612609d78648e42b1f7
HalliS2o <hallist@hey.com>
2024-01-29 17:30:30 +0000

    format on formatting code

--- lua/HalliS20/plugins/formatting.lua
+++ lua/HalliS20/plugins/formatting.lua
@@ -1,37 +1,39 @@
 return {
-  "stevearc/conform.nvim",
-  lazy = true,
-  event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
-  config = function()
-    local conform = require("conform")
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { "prettier", "eslint", "prettierd" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        svelte = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier", "prettierd", "eslint" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
-        lua = { "luaformatter" },
-        python = { "isort", "black" },
-        cpp = { "clang_format" },
-      },
-      --------------- format on save (moved to none-ls) -------------------
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    })
+    "stevearc/conform.nvim",
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettier", "eslint", "prettierd" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                svelte = { "prettier" },
+                css = { "prettier" },
+                less = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier", "prettierd", "eslint" },
+                yaml = { "prettier" },
+                markdown = { "prettier" },
+                graphql = { "prettier" },
+                lua = { "luaformatter" },
+                python = { "isort", "black" },
+                cpp = { "clang_format" },
+                c = { "clang_format" },
+            },
+            --------------- format on save (moved to none-ls) -------------------
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            },
+        })
 
-    ------------------- format key map (moved to none-ls) -------------------
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-      conform.format({ lsp_fallback = true, async = false, timeout_ms = 1000 })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
+        ------------------- format key map (moved to none-ls) -------------------
+        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+            conform.format({ lsp_fallback = true, async = false, timeout_ms = 1000 })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
 }
